0759e8cd67c060da08cb68d4947f60bc64a9fbb9
Eric Schares <38474105+eschares@users.noreply.github.com>
2022-05-19 13:41:58 -0500

    Add files via upload

--- publishing_profiler.py
+++ publishing_profiler.py
@@ -11,9 +11,9 @@ Delivered: Streamlit web app
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import altair as alt
 import time
+import plotly.express as px
 
 
 st.set_page_config(page_title='Publishing Profiler', layout='centered', initial_sidebar_state="expanded")
@@ -23,6 +23,7 @@ st.subheader("Analyze your campus publishing output")
 
 # either Excel or tab delimited are your choices
 # Export WoS data with OG=Iowa State University OR AD=Ames, IA
+# Expecting column headers like DT and RP 
 
 #file = filename = "2020_WoS_1-500_exportedasUTF8.txt"  #works
 file = filename = "WoS_2020_tabdelimited_full_record_1000records.txt"  #seems to work?
@@ -55,10 +56,10 @@ for percent_complete in range(100):
 st.balloons()
 #update this number somehow, maybe each loop is 1/N where N is number of files loading
 
-st.markdown("""---""")
 
 ########  Show information about the raw starting data  ########
-total_loaded_records = df.shape[0]
+st.markdown("""---""")
+total_loaded_records = df.shape[0]      #have to save this number off since we need it later and the df will have changed by then
 st.subheader(f'Raw Data loaded, Web of Science {total_loaded_records} records')
 
 if st.checkbox('Show Raw Data'):
@@ -69,9 +70,10 @@ if st.checkbox('Show Raw Distribution of Document Types'):
 st.markdown("""---""")
 
 
-########  Show information about removing Document Types of Editorials, Corrections, etc  ########
-# Remove the Book Review type since it contains the word "Review"
-df = df.loc[~(df['DT'].isin(['Book Review']))]
+########  Show information about removing Document Types (DT) of Editorials, Corrections, etc  ########
+# First remove the Book Review type since it contains the word "Review", two variants
+df_bookreview_removed = df.loc[(df['DT'].isin(['Book Review', 'Book Review; Early Access']))]
+df = df.loc[~(df['DT'].isin(['Book Review', 'Book Review; Early Access']))]
 
 # Then keep Articles, which show up as: 
 #     Article; 
@@ -80,18 +82,33 @@ df = df.loc[~(df['DT'].isin(['Book Review']))]
 # and keep Reviews, which show up as:
 #     Review
 #     Review; Early Access
-# But not Book Review since we removed that in the previous step
-df = df.loc[(df['DT'].str.contains('Article', case=False, regex=False, na=False)) | (df['DT'].str.contains('Review', case=False, regex=False, na=False))]
+# But NOT Book Review since we removed that in the previous step
 
+# Save what you removed
+# These "removed" lines have to go first, since df gets changed in the next line
+df_removed = df.loc[~((df['DT'].str.contains('Article', case=False, regex=False, na=False)) | (df['DT'].str.contains('Review', case=False, regex=False, na=False)))]
+# Combine the Book Reviews with everything NOT Article, Review
+frames = [df_removed, df_bookreview_removed]
+df_removed = pd.concat(frames)
+
+# Then actually change the df to keep only the wanted DT's
+df = df.loc[(df['DT'].str.contains('Article', case=False, regex=False, na=False)) | (df['DT'].str.contains('Review', case=False, regex=False, na=False))]
 # Sometimes Article Title is repeated from an Early Access version
 df = df.drop_duplicates(subset=['TI'], keep='first', ignore_index=True)
 
-st.subheader(f'Remove {total_loaded_records - df.shape[0]} Editorials, Corrections, Book Reviews etc.')
-st.subheader(f'Keep Article and Review Types, {df.shape[0]} records')
-if st.checkbox('Show Filtered Data'):
+
+st.subheader('Analyze Document Types')
+st.write(f'**Remove** Editorials, Corrections, Book Reviews etc, **{total_loaded_records - df.shape[0]}** records')
+if st.checkbox(f'Show Removed Data, {df_removed.shape[0]}' ):
+    st.write(df_removed)
+if st.checkbox('Show Removed Document Types'):
+    st.write(pd.value_counts(df_removed['DT']))
+
+st.write(f'**Keep** Article and Review Types, **{df.shape[0]}** records')
+if st.checkbox(f'Show Kept Data'):
     st.write(df)
 
-if st.checkbox('Show Filtered Distribution of Document Types'):
+if st.checkbox('Show Kept Document Types'):
     st.write(pd.value_counts(df['DT']))
 
 
@@ -107,8 +124,9 @@ df['ISU_CA'] = CA_column_filt
 
 
 #############  Sidebar Filters  ###############
-st.sidebar.subheader("**Filters**")
+st.sidebar.subheader("**CA Filter**")
 
+st.sidebar.write("Corresponding Author is defined as as RP (reprint) column containing Iowa State Univ; Ames Lab; Ames, IA; or USDA")
 CA_filt = st.sidebar.radio("Look at Corresponding Authors only?", ("Yes", "No"))
 
 if CA_filt == 'Yes':
@@ -122,15 +140,45 @@ st.sidebar.write(f'{df[filt].shape[0]} rows out of {df.shape[0]} match your crit
 df_publisher_list = pd.read_csv('Publisher_list_converter.csv')
 df = pd.merge(df_publisher_list, df, on='PU', how='right')      #like a VLOOKUP in Excel, right merge means output dataframe has all rows from 2nd df and matching rows from 1st df. If rows are not matched in 1st df they replaced by NaN
 
-df.to_csv('PU_cleaned.csv')
-
+df.to_csv('PU_cleaned_windowin.csv', index=False)
 
 
 ########  Charts start here  ########
 st.markdown("""---""")
 st.subheader('Charts')
 
-df[filt]
+pie_counts = df[filt]['PU_cleaned'].value_counts(dropna=True, sort=True)
+df_pie_counts = pd.DataFrame(pie_counts)
+df_pie_counts_reset = df_pie_counts.reset_index()
+df_pie_counts_reset.columns = ['PU_cleaned', 'count']
+df_pie_counts_reset
+df_pie_counts_reset.loc[df_pie_counts_reset['count'] < 3, 'PU_cleaned'] = 'Other' # Represent only publishers with at least 5 pubs that year
+
+fig = px.pie(df_pie_counts_reset, values='count', names='PU_cleaned',
+             title = 'Article count',
+             hover_data=['count'],
+             color='PU_cleaned'
+)
+
+fig
+
+
+by_journal = df[filt].groupby('SO')['PU_cleaned'].describe()
+df_by_journal = pd.DataFrame(by_journal)
+df_by_journal = df_by_journal.reset_index()
+df_by_journal.columns = ['Journal', 'count', 'unique', 'PU_cleaned', 'freq']
+#df_by_journal
+
+fig2 = px.sunburst(df_by_journal, path=['PU_cleaned', 'Journal'], values = 'count', color='PU_cleaned',
+            title = 'Journal titles by publisher',
+            width=800, height=800
+)
+fig2
+
+
+
+
+
 publisher_chart = alt.Chart(df[filt]).mark_bar().encode(
     alt.X('PU_cleaned:N', sort='-y', title='Publisher'),
     alt.Y('count()'),
